749838b3a48f2aad1f16e9546c162264f5c28914
Terry Yao/WHQ/Wistron <Terry_Yao@wistron.com>
2022-10-12 09:05:24 +0800

    build basic render view methods

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+def index(request):
+    # return HttpResponse("hi!!")
+    # return HttpResponse("<h1>big</h1><h4>small</h4>")
+    # return HttpResponse(status=404)
+    return HttpResponseNotFound("tutorial Can't found.")
+
+def articleList(request):
+    return HttpResponse("tutorial Articles List")
+
+def author(request):
+    return render(request, "author.html")
